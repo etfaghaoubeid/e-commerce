5b7d2811654b2113e63ea7e1b057f5eacb1f1231
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-10-14 11:58:40 +0000

    restruct project

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../directory/directory.component";
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default Homepage;
