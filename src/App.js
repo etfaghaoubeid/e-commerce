27cee0ca0403c124e5cdfcdb8899edad7fd64181
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-10-15 14:48:44 +0000

    add Header and signin components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import {Route,Switch } from "react-router-dom"
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component ={Homepage}/>
         <Route exact path="/shop" component ={ShopPage}/>
+        <Route exact path="/sign" component ={SignInAndSignUp}/>
 
         </Switch>    
     </div>
