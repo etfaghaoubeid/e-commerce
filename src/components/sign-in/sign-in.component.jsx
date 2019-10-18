94a6608977f4021a8eef8eae4d6dde068c133ed6
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-10-18 08:48:56 +0000

    add  sign in functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custum-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.util";
+import { signInWithGoogle, auth } from "../../firebase/firebase.util";
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -11,8 +11,18 @@ class SignIn extends React.Component {
       password: ""
     };
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: ""
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handleChange = event => {
     const { name, value } = event.target;
