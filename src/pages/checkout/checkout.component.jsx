1df00e0dc2b45a16380831105f891997a54dfa98
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-10-22 21:07:30 +0000

    calcul total of checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import {
+  selectCartItems,
+  selectTotal
+} from "../../redux/cart-dropdown/cart-dropdown.selector";
 import "./checkout.styles.scss";
-const Chekout = () => <h2>checkout</h2>;
-export default Chekout;
+const Chekout = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description </span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    {cartItems.map(item => (
+      <span>{item.name}</span>
+    ))}
+    <div className="total">
+      <span>Total :${total}</span>
+    </div>
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectTotal
+});
+export default connect(mapStateToProps)(Chekout);
--- src/redux/cart-dropdown/cart-dropdown.selector.js
+++ src/redux/cart-dropdown/cart-dropdown.selector.js
@@ -6,3 +6,4 @@ export const selectCartItems = createSelector([selectCart],cart=>cart.cartItems)
 export const selectIsOpen  = createSelector([selectCart],cart=>cart.isOpen);
 
 export const selectCartItemsCount = createSelector([selectCartItems], cartItems=>cartItems.reduce((accu, item) => accu + item.quantity, 0))
+export const selectTotal = createSelector([selectCartItems], (items)=>items.reduce((acc ,item)=>acc+item.quantity*item.price,0))
